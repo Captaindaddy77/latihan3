d04de5ff19d6c8566fb3bf13f86f6cd7b898f254
Captaindaddy77 <nabilzaky77@gmail.com>
2025-06-12 11:51:46 +0800

    membuat mutator

--- Rumus.java
+++ Rumus.java
@@ -10,5 +10,29 @@ public class BangunDatar {
     int lebar;
     int hasil;
 }
+    // Method untuk menghitung luas persegi
+    public void hitungLuasPersegi() {
+        hasil = sisi * sisi;
+        System.out.println("Luas Persegi = " + hasil);
+    }
 
+    // Method untuk menghitung luas persegi panjang
+    public void hitungLuasPersegiPanjang() {
+        hasil = panjang * lebar;
+        System.out.println("Luas Persegi Panjang = " + hasil);
+    }
+
+    // Contoh main method untuk menjalankan
+    public static void main(String[] args) {
+        BangunDatar bd = new BangunDatar();
+
+        // Contoh hitung luas persegi
+        bd.sisi = 5;
+        bd.hitungLuasPersegi();
+
+        // Contoh hitung luas persegi panjang
+        bd.panjang = 8;
+        bd.lebar = 4;
+        bd.hitungLuasPersegiPanjang();
+    }
 }
