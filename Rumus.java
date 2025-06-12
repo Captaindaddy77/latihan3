84b435b9e4ef3b6692dbbe302aa41af9a93bf7ca
Captaindaddy77 <nabilzaky77@gmail.com>
2025-06-12 11:43:48 +0800

    initial commit

--- Rumus.java
+++ Rumus.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan3;
-
-/**
- *
- * @author acer
- */
 public class Rumus {
-    
+// Nama: [Nama Lengkap]
+// NPM : [Nomor Pokok Mahasiswa]
+// Kelas: [Nama Kelas]
+
 }
