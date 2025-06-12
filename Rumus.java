82654b1231f42c819d7dbf26966a3c29e7573a05
Captaindaddy77 <nabilzaky77@gmail.com>
2025-06-12 11:47:31 +0800

    menambahkan atribut

--- Rumus.java
+++ Rumus.java
@@ -3,5 +3,12 @@ public class Rumus {
 // Nama: [Muhammad Nabil Zaky]
 // NPM : [2310010511]
 // Kelas: [4C REG BJM]
+public class BangunDatar {
+    // Atribut-atribut
+    int sisi;
+    int panjang;
+    int lebar;
+    int hasil;
+}
 
 }
